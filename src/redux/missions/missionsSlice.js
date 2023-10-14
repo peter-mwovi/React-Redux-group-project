a6238a4fea583b2f498f7972156a9c8cde7bdfda
efmomoh <efmomoh25@gmail.com>
2023-10-14 16:28:25 +0000

    Adds missionsSlice.js

--- src/redux/missions/missionsSlice.js
+++ src/redux/missions/missionsSlice.js
@@ -1,16 +1,1 @@
-import { createSlice } from '@reduxjs/toolkit';
-
-const missionsSlice = createSlice({
-  name: 'missions',
-  initialState: {
-    data: [],
-  },
-  reducers: {
-    setMissions: (state, action) => {
-      state.data = action.payload;
-    },
-  },
-});
-
-export const { setMissions } = missionsSlice.actions;
-export default missionsSlice.reducer;
+// missionSlice.js logic goes Headers..
