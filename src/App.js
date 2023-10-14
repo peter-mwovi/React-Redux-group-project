a58b5639285d638d09de7d9b31b28d21c5000e30
efmomoh <efmomoh25@gmail.com>
2023-10-14 16:39:01 +0000

    Modifies App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Profile from './routes/Profile';
+import Rockets from './routes/Rockets';
+import Layout from './routes/Layout';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Spacer App</h1>
-    </div>
-  );
-}
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Rockets />} />
+      {/* missions path */}
+      <Route path="profile" element={<Profile />} />
+    </Route>
+  </Routes>
+);
 
 export default App;
