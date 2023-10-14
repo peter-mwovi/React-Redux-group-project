29a02fa528b4cad5ac01ee74173aaf501ee65764
efmomoh <efmomoh25@gmail.com>
2023-10-14 15:46:48 +0000

    Adds Profile.js file

---
+++ src/routes/Profile.js
@@ -0,0 +1,11 @@
+import ReservedRockets from '../components/ReservedRockets';
+import ReservedMissions from '../components/ReservedMissions';
+
+const Profile = () => (
+  <div className="d-flex p-5 gap-5">
+    {/* ReservedMissions here  */}
+    <ReservedRockets />
+  </div>
+);
+
+export default Profile;
